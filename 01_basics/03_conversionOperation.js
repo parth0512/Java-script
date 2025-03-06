6ff644dfdb5964aaae3a57e0eeb9f6b65b4aba4d
Parth Patel <163419505+parth0512@users.noreply.github.com>
2025-03-06 09:09:51 +0000

    converision

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = '33abc';
+
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//""  => flse
+// "parth" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
