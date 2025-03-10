d820a53bf1aba6d9bf8eb3b65851a222a425e67e
Parth Patel <163419505+parth0512@users.noreply.github.com>
2025-03-10 07:46:58 +0000

    objects done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -19,13 +19,12 @@ const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
 
-
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Parth"))
+console.log(Array.from("Parth"))
+console.log(Array.from({name: "Parth"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); 
