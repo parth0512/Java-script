3365355011e6237cca3ea1769bb3809b9911d170
Parth Patel <163419505+parth0512@users.noreply.github.com>
2025-03-11 05:03:06 +0000

    arrow funtction and iife

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "Parth"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "parth"
+    if (username === "parth") {
         const website = " youtube"
         // console.log(username + website);
     }
